2559f64926f10d75308e6bb99f5f52c6f2f1b607
pushpalatharaja <128318219+pushpalatharaja@users.noreply.github.com>
2024-10-10 23:34:22 +0530

    Update findOccurrence.js

--- week3-ts/findOccurrence.js
+++ week3-ts/findOccurrence.js
@@ -2,7 +2,7 @@ let occurrences = function () {
     let nums = [2, 4, 5, 2, 1, 2];
     let arr = [];
     let count = 1;
-    for (var i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (!arr.includes(nums[i])) {
             arr.push(nums[i]);
         }
@@ -13,12 +13,12 @@ let occurrences = function () {
     console.log(count);
 };
 occurrences();
-/////////////////////////////////////Another Method with Val///////////////
-var occurrencesVal = function () {
-    var nums = [2, 4, 5, 2, 1, 2];
-    var k = 2;
-    var count = 0;
-    for (var i = 0; i < nums.length; i++) {
+/////////////////////////////////////Another Method///////////////
+let occurrencesVal = function () {
+    let nums = [2, 4, 5, 2, 1, 2];
+    let k = 2;
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] == k) {
             count++;
         }
